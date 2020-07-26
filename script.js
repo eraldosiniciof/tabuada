a086b94a96ee58acf1934bfd38f81d2df4edb400
eraldosiniciof <eraldosinicio@gmail.com>
2020-07-26 09:13:55 -0300

    att

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcular() {
+    const textN = document.querySelector("#numero")   
+    const res = document.querySelector("#res")
+
+    const numero = Number(textN.value)
+    res.innerHTML = ''
+    
+    for (let c = 1; c <= 10; c++) {
+        res.innerHTML += `<p>${numero} x ${c} = ${numero * c}</p>`
+    }
+    
+}
